acb17e1782cd015ba7f17b7cfd98514edbf3bafe
Sajan Babu <111737079+bsajanbe@users.noreply.github.com>
2023-02-18 11:47:46 +0530

    Add files via upload

---
+++ Question.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+const Question = ({ title, info }) => {
+    const [showInfo, setInfo] = useState(true)
+    return (
+        <article className='question'>
+            <header>
+                <h4> {title}</h4>
+                <button className='btn' onClick={() => setInfo(!showInfo)}>
+                    {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+
+                </button>
+            </header>
+            {showInfo && <p>{info}</p>}
+
+
+        </article>
+    )
+}
+
+export default Question
